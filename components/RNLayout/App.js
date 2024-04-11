45fa95ff6bafcf25e4a716c3a7a61e24765dcde4
emmanuel-yegon <kptooman@gmail.com>
2024-04-11 10:25:29 +0300

    Learning react-native core components

---
+++ components/RNLayout/App.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import Box from "../CoreComponents/components/RNLayout/Box";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "#8e9b00", top: 75, left: 75 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2 </Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box
+        style={{
+          backgroundColor: "#ab9156",
+          position: "absolute",
+          top: 100,
+          left: 100,
+        }}
+      >
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 6,
+    borderColor: "red",
+    marginTop: 64,
+  },
+});
---
+++ components/RNLayout/Box.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Box({ children, style }) {
+  return (
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#fff",
+    padding: 20,
+    flexGrow: 1,
+    width: 100,
+    height: 100,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+});
